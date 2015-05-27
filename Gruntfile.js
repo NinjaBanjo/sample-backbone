88c2e841f40d302e39807c73a8406e670a5cd894
Derek Persons <derek.persons@gmail.com>
2015-05-27 14:40:02 -0500

    adding less, and updating to fix some layouts:

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,17 +11,25 @@ module.exports = function (grunt) {
         }
       }
     },
+    less:{
+      main: {
+        files: {
+          'static/styles/app.css': ['app/less/main.less']
+        }
+      }
+    },
     watch: {
       main: {
-        files: 'app/**/*',
-        tasks: 'default'
+        files: 'app/**/**',
+        tasks: ['default']
       }
     }
   });
 
-  grunt.registerTask('default', 'browserify');
+  grunt.registerTask('default', 'browserify', 'less');
   grunt.registerTask('run', ['default', 'watch']);
 
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 };
